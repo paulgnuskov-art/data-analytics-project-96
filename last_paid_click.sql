ccbe41dc5241af32af771405943012a5dbe5966e
paulgnuskov-art <paulgnuskov@gmail.com>
2026-05-14 17:38:24 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,21 +1,22 @@
-select s.visitor_id,
-s.visit_date,
-s.source as utm_source,
-s.medium as utm_medium,
-s.campaign as utm_campaign,
-l.lead_id,
-l.created_at,
-l.amount,
-l.closing_reason,
-l.status_id
-from sessions s 
-left join leads l 
-on s.visitor_id = l.visitor_id 
+select
+    s.visitor_id,
+    s.visit_date,
+    s.source as utm_source,
+    s.medium as utm_medium,
+    s.campaign as utm_campaign,
+    l.lead_id,
+    l.created_at,
+    l.amount,
+    l.closing_reason,
+    l.status_id
+from sessions as s
+left join leads as l
+    on s.visitor_id = l.visitor_id
 where s.medium <> 'organic'
-ORDER BY
-    amount DESC NULLS LAST,
-    visit_date ASC,
-    utm_source ASC,
-    utm_medium ASC,
+order by
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
     utm_campaign asc
-    limit 10;
+limit 10;

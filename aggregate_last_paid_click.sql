f14b47a63bd130f1d1703c8e6ed25728c416084a
paulgnuskov-art <paulgnuskov@gmail.com>
2026-05-19 14:11:57 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,18 +1,21 @@
 SELECT
-    s.visit_date,
+    DATE(s.visit_date) AS visit_date,
     s.source AS utm_source,
     s.medium AS utm_medium,
     s.campaign AS utm_campaign,
-    COUNT(s.visitor_id) AS visitors_count,
-    COALESCE(SUM(vk.daily_spent), 0)
-    + COALESCE(SUM(ya.daily_spent), 0) AS total_cost,
+
+    COUNT(DISTINCT s.visitor_id) AS visitors_count,
+
+    MAX(vk.daily_spent) + MAX(ya.daily_spent) AS total_cost,
+
     COUNT(DISTINCT l.lead_id) AS leads_count,
+
     COUNT(
         DISTINCT CASE
             WHEN
                 l.closing_reason = 'Успешно реализовано'
                 OR l.status_id = 142
-                THEN l.lead_id
+            THEN l.lead_id
         END
     ) AS purchases_count,
 
@@ -21,37 +24,36 @@ SELECT
             WHEN
                 l.closing_reason = 'Успешно реализовано'
                 OR l.status_id = 142
-                THEN l.amount
+            THEN l.amount
         END
     ) AS revenue
 
-FROM sessions AS s
+FROM sessions s
 
-LEFT JOIN leads AS l
-    ON
-        s.visitor_id = l.visitor_id
-        AND s.visit_date <= l.created_at
+LEFT JOIN leads l
+    ON s.visitor_id = l.visitor_id
+    AND s.visit_date <= l.created_at
 
-LEFT JOIN vk_ads AS vk
-    ON
-        s.source = vk.utm_source
-        AND s.medium = vk.utm_medium
-        AND s.campaign = vk.utm_campaign
-        AND s.content = vk.utm_content
-        AND DATE(s.visit_date) = vk.campaign_date
+LEFT JOIN vk_ads vk
+    ON s.campaign = vk.utm_campaign
+    AND DATE(s.visit_date) = vk.campaign_date
 
-LEFT JOIN ya_ads AS ya
-    ON
-        s.source = ya.utm_source
-        AND s.medium = ya.utm_medium
-        AND s.campaign = ya.utm_campaign
-        AND s.content = ya.utm_content
-        AND DATE(s.visit_date) = ya.campaign_date
+LEFT JOIN ya_ads ya
+    ON s.campaign = ya.utm_campaign
+    AND DATE(s.visit_date) = ya.campaign_date
 
-WHERE s.medium <> 'organic'
+WHERE s.medium IN (
+    'cpc',
+    'cpm',
+    'cpa',
+    'youtube',
+    'cpp',
+    'tg',
+    'social'
+)
 
 GROUP BY
-    s.visit_date,
+    DATE(s.visit_date),
     s.source,
     s.medium,
     s.campaign
@@ -63,4 +65,5 @@ ORDER BY
     utm_source ASC,
     utm_medium ASC,
     utm_campaign ASC
+
 LIMIT 15;
